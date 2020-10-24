5a747ca6b025a953d03cfc04c1e5fdbe7d2f2279
Pritam Kamble <7276279026.pk@gmail.com>
2020-10-24 18:00:03 +0530

    famer routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'farmer', loadChildren: () => import('./farmer/farmer.module').then(m => m.FarmerModule) }
 ];
 
 @NgModule({
