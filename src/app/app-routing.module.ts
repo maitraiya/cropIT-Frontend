06d7e8732b07bf3a0619042b556a155331e87adb
Pritam Kamble <7276279026.pk@gmail.com>
2020-10-25 19:37:24 +0530

    admin module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'farmer', loadChildren: () => import('./farmer/farmer.module').then(m => m.FarmerModule) }
+  { path: 'farmer', loadChildren: () => import('./farmer/farmer.module').then(m => m.FarmerModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
 ];
 
 @NgModule({
