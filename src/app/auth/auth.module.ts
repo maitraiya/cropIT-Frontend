3be46c98a2c7aeba667c11510d602b846df776ec
pritamkamble <7276279026.pk@gmail.com>
2021-02-27 18:55:25 +0530

    unused components removed

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,15 +4,16 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterStepOneComponent } from './register-step-one/register-step-one.component';
-import { RegisterStepTwoComponent } from './register-step-two/register-step-two.component';
-import { RegisterStepThreeComponent } from './register-step-three/register-step-three.component';
 import { RegisterComponent } from './register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [AuthComponent, LoginComponent, RegisterStepOneComponent, RegisterStepTwoComponent, RegisterStepThreeComponent, RegisterComponent],
+  declarations: [
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule,
